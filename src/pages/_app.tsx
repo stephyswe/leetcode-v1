81f24c8a1a22d060bbc173053fd8c1ffd8da22ad
Stephanie <stephaniiehallberg@gmail.com>
2023-05-28 12:29:35 +0200

    fix(recoil): recoil warning * https://github.com/facebookexperimental/Recoil/issues/733

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,9 @@ import Head from "next/head";
 import { RecoilRoot } from "recoil";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+
+import "@/utils/recoil";
+
 export default function App({ Component, pageProps }: AppProps) {
 	return (
 		<RecoilRoot>
